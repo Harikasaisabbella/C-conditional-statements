8a9463d4f6d179b79fcd562190735d1ce5f5d443
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-09-21 12:23:48 +0530

    Add files via upload

---
+++ two integers equal or not.c
@@ -0,0 +1,22 @@
+/* C program to find two integers are equal or not */
+#include<stdio.h>
+void main()
+{  // Declaring the variables
+    int number1, number2;
+    
+    // Taking inputs
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &number1, &number2);
+    
+    // Printing the results
+    
+    if(number1 == number2)
+     {
+	   printf("number1 and number2 are equal\n");
+}   
+	else
+	{  printf("number1 and number2 are not equal");
+	}
+		     
+	   
+}
